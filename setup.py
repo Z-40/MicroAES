11231d8eab8e904f9d509a6c9923bc5fb33d9c10
Z-40 <58406y4@gmail.com>
2021-06-15 18:52:08 +0530

    Update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ DESCRIPTION = "A pure python implementation of the AES encryption algorithm"
 
 setup(
     name="micro_aes",
-    version="2021.1",
+    version="2021.2",
     license="GNU General Public License Version 3.0",
     author="Z-40",
     description=DESCRIPTION,
