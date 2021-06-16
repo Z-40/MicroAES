73557a9fdb293b12d3931e0c6d3bbef016acec95
Z-40 <58406y4@gmail.com>
2021-06-16 15:20:59 +0530

    Update version

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ DESCRIPTION = "A pure python implementation of the AES encryption algorithm"
 
 setup(
     name="micro_aes",
-    version="2021.3",
+    version="2021.4",
     license="GNU General Public License Version 3.0",
     author="Z-40",
     description=DESCRIPTION,
     url="https://github.com/Z-40/MicroAES",
     install_requires=[],
-    keywords=["AES", "encryption", "cryptography"],
+    keywords=["AES", "cryptography"],
     packages=["micro_aes"],
     license_file="MicroAES/license.txt"
 )
